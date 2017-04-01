3521547607721662c1d9c8b5bd5772247ecb34a7
Alejandro Trevino <atrevino@olivetcollege.edu>
2017-04-01 16:11:05 -0400

    memberexists, function laid out

---
+++ helpers/memberexists.cpp
@@ -0,0 +1,11 @@
+//linear search method to confirm that member exsits
+
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+bool memberexists(const char *filename){
+        std::ifstream ifile(filename);
+        }
