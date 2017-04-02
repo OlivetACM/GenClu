1f022302a3e09edc829290b236b160c5ab0014d8
Grant <Garras@olivetcollege.edu>
2017-04-02 11:54:55 -0400

    Needs revision

--- helpers/load_members.cpp
+++ helpers/load_members.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <cstring>
 #include <vector>
 #include <iterator>
-
 using namespace std;
 
 template<typename Out>
@@ -16,21 +16,35 @@ void split(const string &s, char delim, Out result) {
     }
 }
 
-string[] load_members(string filename){
-	string[] members = new string[];
-	string line = '';
-	int count = 0;
+void chomp(string &s){
+    int pos;
+    if((pos = s.find("\n")) != string::npos)
+    	s.erase(pos);
+}
+
+vector<string> load_members(string filename){
+	vector<string> members;
+	string line;
+	string header;
 	ifstream rfile(filename.c_str());
 	if(rfile.is_open()){
+		getline(rfile, header);
 		while(!rfile.eof()){
+			string[] words = new string[3];
 			rfile >> line;
-			// To be revised
-			line = line.strip();
-			member[count] = split(line, ',');
-			count++;
+			chomp(line);
+			cout << line << endl;
+			split(line, ',', words);
+			members.push_back(words)
 		}
 	}
 	rfile.close();
-	return members
+	return members;
 }
 
+int main(){
+	vector<string> test = load_members("../members.csv");
+	for(int i = 0; i < test.size(); i++)
+		cout << test[i] << endl;
+	return 0;
+}
