d7bf76d53827a4e7e003135dc36acd0ec5c86153
Noah Bailey <nbailey@olivetcollege.edu>
2017-04-02 13:25:52 -0700

    Added header files for respective functions

---
+++ headers/writemember.h
@@ -0,0 +1,10 @@
+#ifndef WRITEMEMBER_H
+#define WRITEMEMBER_H
+#include <iostream>
+#include <fstream>
+#include <sting>
+#include <vector>
+
+bool writemember(std::vector<std:string> name, std::string filename);
+
+#endif
