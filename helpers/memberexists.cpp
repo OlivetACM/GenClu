988983efbde9bb465cb465824aa8ee8f2a948168
xejnayr <xejnayr@gmail.com>
2017-04-02 14:27:09 -0400

    Finalized memberexists function with working test run.

--- helpers/memberexists.cpp
+++ helpers/memberexists.cpp
@@ -1,11 +1,48 @@
 //linear search method to confirm that member exsits
-
 #include<iostream>
-#include<fstream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
-bool memberexists(const char *filename){
-        std::ifstream ifile(filename);
+int memberexists(vector<vector<string>> members, string idnum){
+//Takes a Vector of a String Vector
+//Returns index at with idnum is found, or -1 if idnum is not found
+        for(int i = 0; i < members.size(); i++){
+            
+            if((members.at(i).at(0)) == idnum){
+                return i;
+            }
         }
+        return -1;
+}
+
+
+//Working test run
+/*
+int main()
+{
+    vector<vector<string>> members;
+    vector<string> memtest;
+    vector<string> testmem;
+
+    string idnum = "110222";
+
+    memtest.push_back({"222011"});
+    memtest.push_back({"Name"});
+    memtest.push_back({"Generic"});
+
+    members.push_back({memtest});
+
+    testmem.push_back({"110222"});  //Add info to inner vector
+    testmem.push_back({"J"});       //Sample info, form: (ID, Last Name, First Name)
+    testmem.push_back({"Ryan"});
+
+    members.push_back({testmem});   //Add inner vector to outer vector
+
+    cout<<memberexists(members, idnum); //Returns 1 for this example
+
+    return 0;
+}
+*/
+
