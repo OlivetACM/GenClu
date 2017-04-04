a4fdceb2dce87dced5e198f65278f135bf88e6fc
Kory Slaby <kory.slaby@gmail.com>
2017-04-04 10:35:02 -0400

    Fully working function. Checks for up to 9 events in the current day. Only thing is I don't know all the using so I left the using namespace and comented out the usings i could think of. Other than that it is 100% done and working!

--- helpers/checkprevious.cpp
+++ helpers/checkprevious.cpp
@@ -3,8 +3,10 @@
 #include <ctime>
 #include <fstream>
 #include <map>
+#include <sstream>
 
-//using  std::string;, using std::ctime;
+//using std::string;, using std::ctime;, using std::cout;
+//using std::endl;, using std::map;
 using namespace std;
 
 
@@ -15,7 +17,10 @@ void checkprevious()
 	time_t now = time(0);
 	char* dt = ctime(&now);
 
-	map<string, string> month
+//----------------------------------------------------------------------------
+//this part of the code creats a takes the time I got from c time and peices together a yyymmdd
+//that is stored in the present day variable.
+	map<string, string> month//converts abreviated month part of ctime into its number form.
 	{
 		{"Jan", "01"},
 		{"Feb", "02"},
@@ -30,50 +35,59 @@ void checkprevious()
 		{"Nov", "11"},
 		{"Dec", "12"},
 	};
-	map<string, string>::iterator itsa;
+	map<string, string>::iterator itsa;		
 
-	string day = "000";
+	string day = "000";//where I will store "Mar"
 
 	for(int i = 0; i < 3; i++)
 	{
-		day[i] = dt[4+i];
+		day[i] = dt[4+i];//fils day with the apropriat letters.
 	}
 
-	itsa = month.find(day);
+	itsa = month.find(day);//finds day in the map so that the conversion can begin.
 
 	for(int i = 0; i < 4; i++)
 	{
-		present_day[i] = dt[20+i]; // year
+		present_day[i] = dt[20+i]; //puts the year into present_day 20170000
 	}
 	
 	for(int i = 4; i < 6; i++)
 	{
-		present_day[i] = itsa->second[0 + (i-4)];//month
+		present_day[i] = itsa->second[0 + (i-4)];//puts the month into present_day 20170400
 	}
 
 	for(int i = 6; i < 8; i++)
 	{
-		present_day[i] = dt[8+(i-6)];//day
+		present_day[i] = dt[8+(i-6)];//puts the day into present_day 20170404.which will be our first part of our read in file.
 	}
+//-------------------------------------------------------------------------------------------
+//This part of the code will peace together the rest of the file_location that is needed.
+//for testing perpaces I aimed for 20170404/event1.csv, because I created a event1.csv in a directory 20170404.
+//event1.csv is just the starting event the program will look for.
 
-	file_location = file_location + present_day;
-	file_location = file_location + "/";
-	file_location = file_location + "event1.csv";
-	ifstream file(file_location);
+	file_location = file_location + present_day;//adds the present day part 20170404.
+	file_location = file_location + "/";//20170404/
+	file_location = file_location + "event1.csv";//20170404/event1.csv
+	ifstream file(file_location);//searchs for the now complete file location. or so the one I tested for it can be changed.
 
-	if(file.is_open())
+	if(file.is_open())//This next part will check to see if there is more that one event in the present day directory(20170404).
 	{
 		string tempfilecheck = file_location;
-		for(int i = 1; i < 100; i++)
+		for(int i = 1; i < 10; i++)
 		{
-			tempfilecheck[14] = str(i);// should run through and check for more than one event file
-			ifstream file2(tempfilecheck);//I have to figure out how to turn a int into string and will be all set.
-			cout << tempfilecheck << endl;
+			//----------------------------
+			stringstream ss;	
+			ss << i;				//used in integer to string conversion.
+			string nums = ss.str();
+			//-----------------------------
+			tempfilecheck[14] = nums[0];//changes the event1.csv into event2.csv or 3,4,5,etc. 
+			ifstream file2(tempfilecheck);
 			if(file2.is_open())
 			{
+				//does nothing so it goes through the for loop again and checks for anothe event.csv file.
 			}else{
-				cout << "there are curently " << i << " events made today" << endl;
-				i = 100;
+				cout << "there are curently " << (i-1) << " events made today" << endl;//tells the user how many events there are.
+				i = 100;//ends the for loop.
 			}
 		}
 
@@ -81,9 +95,9 @@ void checkprevious()
 	}else{
 		cout << "there is no event for today yet" << endl;
 	}
-
-
 }
+
+
 
 int main()
 {
