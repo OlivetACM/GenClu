cbf50e8cbdeaa10da7784b9bbb69ad69b1e774e2
xejnayr <xejnayr@gmail.com>
2017-05-12 16:45:02 -0400

    Manual entry header and GUI files

---
+++ headers/manualentries.h
@@ -0,0 +1,14 @@
+#include <QWidget>
+#include <QLineEdit>
+#include <QGridLayout>
+#include <QLabel>
+#include <QPushButton>
+
+class manualEntry : QWidget {
+    Q_OBJECT
+
+    public:
+        manualEntry(QWidget *parent = 0);
+    private:
+
+};
