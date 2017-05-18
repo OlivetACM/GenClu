e88447f988c9aaaac6c8b547218014fb90d0d578
Carlos Barajas <cbarajas@olivetcollege.edu>
2017-05-17 20:36:06 -0400

    no need for these extras

--- headers/memberexists.h
+++
@@ -1,9 +0,0 @@
-#ifndef memberexists_h
-#define memberexists_h
-
-#include<string>
-#include<vector>
-
-int memberexists(std::vector<std::vector<std::string>> members, std::string idnum);        
-
-#endif
